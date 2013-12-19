90cd92c40a360c965fbc2ee41ebefa133b9624c5
ichrak <ichrak@MacBook-Pro-de-ichrak.local>
2013-12-19 11:09:08 +0100

    Initial Commit

---
+++ Task/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Task
+//
+//  Created by ichrak on 19/12/13.
+//  Copyright (c) 2013 ichrak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Task/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Task
+//
+//  Created by ichrak on 19/12/13.
+//  Copyright (c) 2013 ichrak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
